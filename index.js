11e06970af8c0b8bdb457d2b5fc45d9f77ffdebd
DanTerra45 <daniel.terrazas@ucb.edu.bo>
2023-10-23 15:14:48 -0400

    Few fixes and crediting the framework used (MD3)

---
+++ index.js
@@ -0,0 +1,6 @@
+import '@material/web/all.js';
+
+/* Ripple Effect */
+const ripple = document.querySelector('#ripple');
+const control = document.querySelector('#ripple-control');
+ripple.attach(control);
